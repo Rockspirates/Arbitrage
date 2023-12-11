19c13db24be980b595ef0a60f37241a05db3e66d
Rockspirates <aakshathreddy@gmail.com>
2023-12-11 19:57:00 +0530

    updated sort.h

--- sort.h
+++ sort.h
@@ -7,18 +7,13 @@ void mergepairs(vector<pair<string, int>>& arr, int l, int m, int r) {
     int n1 = m - l + 1;
     int n2 = r - m;
     vector<pair<string, int>> L(n1), R(n2);
-
-    // Copy data to temporary vectors L[] and R[]
     for (int i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (int j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
-
-    // Merge the temporary vectors back into arr[l..r]
     int i = 0; // Initial index of first subvector
     int j = 0; // Initial index of second subvector
     int k = l; // Initial index of merged subvector
-
     while (i < n1 && j < n2) {
         if (L[i].first <= R[j].first) {
             arr[k] = L[i];
@@ -29,15 +24,11 @@ void mergepairs(vector<pair<string, int>>& arr, int l, int m, int r) {
         }
         k++;
     }
-
-    // Copy the remaining elements of L[], if there are any
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
-
-    // Copy the remaining elements of R[], if there are any
     while (j < n2) {
         arr[k] = R[j];
         j++;
@@ -48,132 +39,9 @@ void mergepairs(vector<pair<string, int>>& arr, int l, int m, int r) {
 // Merge sort function
 void mergeSortpairs(vector<pair<string, int>>& arr, int l, int r) {
     if (l < r) {
-        // Same as (l+r)/2, but avoids overflow for large l and r
         int m = l + (r - l) / 2;
-
-        // Sort first and second halves
         mergeSortpairs(arr, l, m);
         mergeSortpairs(arr, m + 1, r);
-
-        // Merge the sorted halves
         mergepairs(arr, l, m, r);
-    }
-}
-
-void merge(vector<uint64_t>& arr, int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
-
-    // Create temporary vectors
-    vector<int> L(n1), R(n2);
-
-    // Copy data to temporary vectors L[] and R[]
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
-
-    // Merge the temporary vectors back into arr[l..r]
-    int i = 0; // Initial index of first subvector
-    int j = 0; // Initial index of second subvector
-    int k = l; // Initial index of merged subvector
-
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    // Copy the remaining elements of L[], if there are any
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    // Copy the remaining elements of R[], if there are any
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-
-// Merge sort function
-void mergeSort(vector<uint64_t>& arr, int l, int r) {
-    if (l < r) {
-        // Same as (l+r)/2, but avoids overflow for large l and r
-        int m = l + (r - l) / 2;
-
-        // Sort first and second halves
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-
-        // Merge the sorted halves
-        merge(arr, l, m, r);
-    }
-}
-
-void mergeString(std::vector<std::string>& arr, int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    // Create temporary vectors
-    std::vector<std::string> L(n1);
-    std::vector<std::string> R(n2);
-
-    // Copy data to temporary vectors L[] and R[]
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
-
-    // Merge the temporary vectors back into arr[left..right]
-    int i = 0; // Initial index of first subarray
-    int j = 0; // Initial index of second subarray
-    int k = left; // Initial index of merged subarray
-
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    // Copy the remaining elements of L[], if there are any
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    // Copy the remaining elements of R[], if there are any
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-
-void mergeSortString(std::vector<std::string>& arr, int left, int right) {
-    if (left < right) {
-        // Same as (left+right)/2, but avoids overflow for large left and right
-        int mid = left + (right - left) / 2;
-
-        // Sort first and second halves
-        mergeSortString(arr, left, mid);
-        mergeSortString(arr, mid + 1, right);
-
-        // Merge the sorted halves
-        mergeString(arr, left, mid, right);
     }
 }
